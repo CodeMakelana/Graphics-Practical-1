be7e6e59c2401724bc864b7f2a313d29bbca10e4
kodeki <u23588579@tuks.co.za>
2026-02-17 18:10:27 +0200

    Completed Vector and ready to test

--- Vector.cpp
+++ Vector.cpp
@@ -3,7 +3,7 @@ Vector<n>::Vector() {
     arr = new float[n];
     for (int i = 0; i < n; i++)
     {
-        arr[i] = 0.0f;
+        arr[i] = 0.0;
     }
 }
 
@@ -16,7 +16,7 @@ Vector<n>::Vector(std::initializer_list<float> list) {
         arr[i++] = val;
     }
     for (; i < n; i++) {
-        arr[i] = 0.0f;
+        arr[i] = 0.0;
     }
 }
 
@@ -31,4 +31,115 @@ Vector<n>::~Vector() {
 }
 
 template<int n>
-Vector<n>::Vector(const Vector<n> &v) {}
+Vector<n>::Vector(const Vector<n> &v) {
+    arr = new float[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = v[i];
+    }
+}
+
+template<int n>
+Vector<n>::operator Matrix<n,1>() const {
+    Matrix<n,1> mat;
+    for (int i = 0; i < n; i++) {
+        mat[i][0] = arr[i];
+    }
+    return mat;
+}
+
+template<int n>
+Vector<n>& Vector<n>::operator=(const Vector<n>& v) {
+    if (this == &v) {
+        return *this;
+    }
+    delete[] arr;
+    arr = new float[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = v[i];
+    }
+    return *this;
+}
+
+template<int n>
+Vector<n> Vector<n>::operator+(const Vector<n> v) const {
+    Vector<n> res;
+    for (int i = 0; i < n; i++) {
+        res[i] = arr[i] + v[i];
+    }
+    return res;
+}
+
+template<int n>
+Vector<n> Vector<n>::operator-(const Vector<n> v) const {
+    Vector<n> res;
+    for (int i = 0; i < n; i++) {
+        res[i] = arr[i] - v[i];
+    }
+    return res;
+}
+
+
+template<int n>
+Vector<n> Vector<n>::operator*(const float x) const {
+    vector<n> res;
+    for (int i = 0; i < n; i++) {
+        res[i] = this[i] * x;
+    }
+}
+
+//dot product of two vectors
+template<int n>
+float Vector<n>::operator*(const Vector<n> v) const {
+    float res = 0.0;
+    for (int i < 0; i < n; i++) {
+        res = arr[i] + v[i]
+    }
+    retrun res;
+}
+
+template<int n> 
+float Vector<n>::magnitude() const {
+    float mag = 0.0;
+    float mag = 0.0;
+    for (int i = 0; i < n; i++) {
+        mag += arr[i]*arr[i];
+    }
+    return sqrt(mag);
+}
+
+template<int n>
+Vector<3> Vector<n>::crossProduct(const Vector<3> v) const {
+    vector<3> CrossRes;
+// arr = A and v = B
+// A x B = ( a2b3 - a3b2)i - (a1b3 - a3b1)j + (a1b2 - a2b1)k
+
+    CrossRes[0] = ((arr[1] * v[2]) - (arr[2] * v[1]));
+    CrossRes[1] = -((arr[0] * v[2]) - (arr[2] * v[0]));
+    CrossRes[2] = ((arr[0] * v[1]) - (arr[1] * v[0]));
+    
+    return CrossRes;
+}
+
+template<int n>
+Vector<n> Vector<n>::unitVector() const {
+    //get norm
+    float norm = magnitude();
+    // if (norm == 0) {
+    //     return;
+    // }
+    float inverse = 1 / norm;
+    return (*this) * inverse;
+}
+
+template<int n>
+int Vector<n> getN() const {
+    int count =  0;
+
+    for (int i < 0; i < n; i++) {
+        if (arr[i] != null) {
+            count++
+        }
+    }
+    return count;
+}
+
