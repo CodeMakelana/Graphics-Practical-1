3ef0a80eb7258d2de3a63d6051d8d12729544e0c
kodeki <u23588579@tuks.co.za>
2026-02-19 00:01:13 +0200

    Matrix

---
+++ Matrix.cpp
@@ -0,0 +1,113 @@
+template <int n, int m>
+Matrix<n,m>::Matrix() {
+    arr = new float*[n]; 
+
+    // Combine allocation and initialization for efficiency and correctness
+    for (int i = 0; i < n; i++) {
+        arr[i] = new float[m];
+        
+        // Initialize values for this row
+        for(int j = 0; j < m; j++) {
+            arr[i][j] = 0.0f;
+        }
+    }
+}
+
+template <int n, int m>
+Matrix<n,m>::Matrix(float ** mat1) {
+    arr = mat1;
+}
+
+template<int n,int m>
+Matrix<n,m>::Matrix(const Matrix<n,m> &mat) {
+    arr = new float*[n];
+
+    //creating the new array
+    for (int i = 0; i < n, i++) {
+        arr[i] = new float[m];
+    }
+
+
+    //Deep copying the array
+    for (int i = 0; i < n, i++) {
+        for (int j = 0; j < m; j++) {
+            arr[i][j] = mat[i][j];
+        }
+    }
+}
+
+template<int n, int m>
+Matrix<n,m>::~Matrix() {
+    for (int i = 0; i < n, i++) {
+        delete[] arr [i];
+    }
+
+    delete[] arr;
+    arr = nullptr;
+}
+
+template<int n, int m>
+Matrix<n,m>& Matrix<n,m>::operator=(const Matrix<n,m>& mat) {
+    if (this == &mat) {
+        return *this;
+    }
+
+    // Deallocate existing memory
+    for (int i = 0; i < n; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+    // Allocate new memory
+    arr = new float*[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new float[m];
+    }
+
+    // Deep copy the values
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            arr[i][j] = mat[i][j];
+        }
+    }
+    return *this;
+}
+
+template<int n, int m>
+template<int a>
+Matrix<n,a> Matrix<n,a>::operator*(const Matrix<m,a> mat) const{
+    //Create new matrix to store result in
+    Matrix<m,a> res;
+    for (int i = 0; i < mat.n; i++) {
+        for (int j = 0; j < mat.a; j ++) {
+            for (int k = 0; k < a; k++) {
+                res[i][j] += arr[i][k] * mat[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+template<int n, int m>
+template<int a>
+Matrix<n,m> Matrix<n,m>::operator*(const float s) const {
+    Matrix<n,m> res;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            res[i][j] = arr[i][j] * s;
+        }
+    }
+    return res;
+}
+
+template<int n, int m>
+Matrix<n,m> Matrix<n,m>::operator+(const Matrix<n,m>& mat) const {
+    Matrix<n,m> res;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            res[i][j] = arr[i][j] + mat[i][j];
+        }
+    }
+    return res;
+}
